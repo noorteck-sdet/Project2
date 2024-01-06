c30672f2cc5b10dd0a6d890349f1a7d2cbf1c0b9
zalkh <zalkh@10.0.0.55>
2024-01-06 16:46:05 -0600

    add wrong code

--- src/demo2/TestDemo2.java
+++ src/demo2/TestDemo2.java
@@ -4,5 +4,18 @@ public class TestDemo2 {
 
 	public static void main(String[] args) {
 		System.out.println("Test 2");
+
+	}
+
+	public void message(String send) {
+
+		System.out.println("Good Job Team 2 Keep practicing");
+		
+		int a ;
+		int b 
+		
+		System.out.println(a+c);
+
+		// By: Zain
 	}
 }
